5dfc9552970328f969adb257a114e4f28cbf3042
Vinícius Noda <noda.94@gmail.com>
2024-06-11 11:17:13 -0300

    First commit

---
+++ script/script.js
@@ -0,0 +1,25 @@
+import {productList, expandFlavors} from "./memory.js";
+import { calculateCardsPerPage, createCard, createPage } from "./structure.js";
+
+const createPagesAndCards = () => {
+    const mainContainer = document.getElementById("main-container");
+    const { rows, columns } = calculateCardsPerPage();
+    const cardPerPage = rows * columns;
+    const expandedProductList = expandFlavors(productList).flat();
+    const pagesNeeded = Math.ceil(expandedProductList.length / cardPerPage);
+
+    for (let i = 0; i < pagesNeeded; i++) {
+        const page = createPage();
+        const lower = i * cardPerPage;
+        const upper = (i + 1) * cardPerPage > expandedProductList.length ? expandedProductList.length : (i + 1) * cardPerPage;
+        const slice = expandedProductList.slice(lower, upper);
+
+        slice.forEach(prod => {
+            page.firstChild.appendChild(createCard(prod));
+        });
+
+        mainContainer.appendChild(page);
+    }
+};
+
+createPagesAndCards();
---
+++ script/structure.js
@@ -0,0 +1,61 @@
+const pageWidthInmm = 297;
+const pageHeightInmm = 209;
+const pagePaddingInmm = 5;
+const cardWidthInmm = 70;
+const cardHeightInmm = 55;
+
+export const createCard = (productInfo) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+
+    const brigadeiroImg = document.createElement("img");
+    brigadeiroImg.classList.add("brigadeiro-logo");
+    brigadeiroImg.src = "img/brigadeiro.svg";
+    cardDiv.appendChild(brigadeiroImg);
+
+    const nameP = document.createElement('p');
+    nameP.classList.add("text", "product-name");
+    nameP.textContent = productInfo.name;
+    cardDiv.appendChild(nameP);
+
+    const decorationImg = document.createElement("img");
+    decorationImg.classList.add("decorations");
+    decorationImg.src = "img/decoration.svg";
+    cardDiv.appendChild(decorationImg);
+
+    if (!!productInfo.description) {
+        const descriptionP = document.createElement('p');
+        descriptionP.classList.add("text", "product-description");
+        if (productInfo.description.length > 50) {
+            descriptionP.classList.add("product-description-small");
+        }
+        descriptionP.textContent = productInfo.description;
+        cardDiv.appendChild(descriptionP);
+    }
+
+    return cardDiv;
+};
+
+export const createPage = () => {
+    const page = document.createElement("section");
+    page.classList.add("sheet", "padding-5mm");
+
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("card-container");
+    page.appendChild(cardContainer);
+
+    return page;
+};
+
+export const calculateCardsPerPage = () => {
+    const workingWidth = pageWidthInmm - (2 * pagePaddingInmm);
+    const workingHeight = pageHeightInmm - (2 * pagePaddingInmm);
+
+    const rows = Math.floor(workingWidth / cardWidthInmm);
+    const columns = Math.floor(workingHeight / cardHeightInmm);
+
+    return {
+        rows,
+        columns
+    };
+};
